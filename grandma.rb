b9dc3e741cac0cb4282d83ffb438d6ef06a11506
Aliya Lewis <aliyalewis5@gmail.com>
2019-04-25 19:44:42 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,9 @@
-def speak_to_grandma(phrase)
-  if phrase == phrase.downcase
+def speak_to_grandma(string)
+  if string == string.downcase
     puts "HUH?! SPEAK UP, SONNY!"
-  elsif phrase == phrase.uppercase
+  elsif string == string.uppercase
     puts "NO, NOT SINCE 1938!"
-  else phrase = "I LOVE YOU GRANDMA!"
+  else string = "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
   end
 
