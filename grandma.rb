8beec5fdc609c26992b97eb4a2bce1b43b4835de
Aliya Lewis <aliyalewis5@gmail.com>
2019-04-25 19:43:46 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -5,6 +5,7 @@ def speak_to_grandma(phrase)
     puts "NO, NOT SINCE 1938!"
   else phrase = "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
+  end
 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
